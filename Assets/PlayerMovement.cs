5f58d65c69239382797198dffbd679874b2a2a4e
OctavioSpano <101335584+OctavioSpano@users.noreply.github.com>
2022-06-02 14:32:11 -0300

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+    }
+    void OnCollisionExit(Collision col2)
+    {
+        if (col2.gameObject.name == "Plane")
+        {
+            transform.position = new Vector3(0, 0.5f, 0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TPFinalJuego
+ 
